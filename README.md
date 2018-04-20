bb4ee405fa07ad72860d975f1efcbdb757d8d6a7
Łukasz Wójcik <kontakt@lukaszwojcik.net>
2018-04-21 00:25:54 +0200

    adding project

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+This is an archival copy of my Polish blog I used to maintain from 2007 to 2009. I keep it here entirely for nostalgic reasons.
---
+++ portfolio/wlasny-szablon-wordpress/index.php
@@ -0,0 +1,3 @@
+<?php header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://www.escapemagazine.pl/369684-wlasny-szablon-wordpress");
+header("Connection: close"); exit; ?>
