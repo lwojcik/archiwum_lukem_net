40f33d35fe975bb501e8a8581336882400f18899
Łukasz Wójcik <kontakt@lukaszwojcik.net>
2018-04-21 18:37:09 +0200

    removing dead links

--- portfolio/wlasny-szablon-wordpress/index.php
+++ portfolio/wlasny-szablon-wordpress/index.php
@@ -1,3 +1,1 @@
-<?php header("HTTP/1.1 301 Moved Permanently");
-header("Location: http://www.escapemagazine.pl/369684-wlasny-szablon-wordpress");
-header("Connection: close"); exit; ?>
+<meta http-equiv="refresh" content="0; url=http://www.escapemagazine.pl/369684-wlasny-szablon-wordpress" />
--- weblog/index.php
+++ weblog/index.php
@@ -1,3 +1,1 @@
-<?php header("HTTP/1.1 301 Moved Permanently");
-header("Location: http://lukaszwojcik.net/");
-header("Connection: close"); exit; ?>
+<meta http-equiv="refresh" content="0; url=https://lukaszwojcik.net/" />
